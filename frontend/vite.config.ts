39615e854f3652d3661dec85a8bc36c78c39ada7
Gopal Kundu <kundugopal2005@gmail.com>
2026-08-04 10:51:15 +0530

    Backend URL Added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-let url = "https://fitness-challenge-tracking-platform-beta.vercel.app",
-// let url = 'http://localhost:5000',
+let url = "https://fitness-challenge-tracking-platform-beta.vercel.app"
+// let url = 'http://localhost:5000'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
